d626394308ef9a04557fe95c8eca4d2ebb4cbcc4
ChizhAndrey <chizhelicovandrey@gmail.com>
2023-04-22 16:58:24 +0300

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,33 @@
 /// <reference path="cypress/support/index.d.ts" />
 import { defineConfig } from "cypress";
-import fs from "fs/promises";
+import fs from "fs-extra";
 
 export default defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
             on("task", {
-                async deleteFolder(folderPath) {
-                    await fs.rmdir(folderPath, {maxRetries: 10, recursive: true })
+                async deleteFolder(folderPath: fs.PathLike) {
+                    await fs.rmdir(folderPath, {maxRetries: 10, recursive: true });
                     return null;
                 }         
+            }),
+            on("task", {
+                async deleteFolderContents(folderPath: string) {
+                    await fs.emptyDir(folderPath);
+                    return null;
+                }
+            }),
+            on("task", {
+                async writeDataToFile({path, data}: {path: number | fs.PathLike, data: string | NodeJS.ArrayBufferView}) {
+                    await fs.writeFile(path, data);
+                    return null;
+                }
+            }),
+            on("task", {
+                async copyFile({src, dst, flags}: {src: fs.PathLike, dst: fs.PathLike, flags?: number}) {
+                    await fs.copyFile(src, dst, flags);
+                    return null;
+                }
             })
         },
         
