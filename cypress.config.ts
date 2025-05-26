6d79a5aaf94a501ec485655a03d2599e9ab3ec13
ChizhAndrey <chizhelicovandrey@gmail.com>
2025-05-26 15:55:34 +0300

    add cypress config

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+    e2e: {      
+        baseUrl: "https://demoqa.com/",
+        viewportHeight: 1080,
+        viewportWidth: 1920,
+        blockHosts: [
+            "www.googletagservices.com",
+            "pagead2.googlesyndication.com",
+            "stats.g.doubleclick.net",
+            "www.google-analytics.com",
+        ],
+        specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
+    },
+});
